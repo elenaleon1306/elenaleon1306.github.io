c09f57e9f8a43c89db5c7ed4333a06b878e2312b
elenaleon1306 <109203179+elenaleon1306@users.noreply.github.com>
2022-07-13 13:27:09 +0200

    Add files via upload

---
+++ clases.js
@@ -0,0 +1,38 @@
+function openNav() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function lengua() {
+  window.open("./lengua.html")
+}
+function matematicas() {
+  window.open("./matematicas.html")
+}
+function ingles() {
+  window.open("./ingles.html")
+}
+function fyq() {
+  window.open("./fyq.html")
+}
+function byg() {
+  window.open("./byg.html")
+}
+function gyh() {
+  window.open("./gyh.html")
+}
+function dibujotecnico() {
+  window.open("./dibujotecnico.html")
+}
+function arts() {
+  window.open("./arts.html")
+}
+function educacionfisica() {
+  window.open("./educacionfisica.html")
+}
+function religion() {
+  window.open("./religion.html")
+}
